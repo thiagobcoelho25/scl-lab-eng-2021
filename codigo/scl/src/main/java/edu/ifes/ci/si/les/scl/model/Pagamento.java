eed16fdefde774d106c40efee5e80bc5800cf1ff
IsraelTuao <israeleduardomonteiro@gmail.com>
2021-06-12 10:53:35 -0300

    Atividade 7 & 8 - Criação Pagamento

--- codigo/scl/src/main/java/edu/ifes/ci/si/les/scl/model/Pagamento.java
+++ codigo/scl/src/main/java/edu/ifes/ci/si/les/scl/model/Pagamento.java
@@ -2,18 +2,52 @@ package edu.ifes.ci.si.les.scl.model;
 
 import java.io.Serializable;
 
-import edu.ifes.ci.si.les.scl.model.enums.TipoFormaPagamento;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 
+import edu.ifes.ci.si.les.scl.model.enums.TipoFormaPagamento;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Entity
 public class Pagamento implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
+	
+	@DecimalMin(value = "0.00")
+	@Digits(integer = 6, fraction = 2)
 	private Double valor;
 
+	@DecimalMin(value = "0.00")
+	@Digits(integer = 6, fraction = 2)
 	private Double desconto;
 
+	@Enumerated(EnumType.STRING)
 	private TipoFormaPagamento formaDePagamento;
+	
+	@OneToOne
+	@JoinColumn(name = "pedido_id")
+	private Pedido pedido;
 
 }
