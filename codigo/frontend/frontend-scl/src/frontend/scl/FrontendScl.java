1ea729e6dc0e8d4231974ff4430cb93f71f8a617
Threads-creator <thiago2.bsp14@gmail.com>
2021-06-23 15:20:18 -0300

    modificando View CadastroPedido

--- codigo/frontend/frontend-scl/src/frontend/scl/FrontendScl.java
+++ codigo/frontend/frontend-scl/src/frontend/scl/FrontendScl.java
@@ -3,10 +3,15 @@ package frontend.scl;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class FrontendScl extends Application {
     
@@ -24,8 +29,16 @@ public class FrontendScl extends Application {
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
+
+        URL arquivoFXML = getClass().getResource("/views/FXMLCadastroPedido.fxml");
+
+        Parent fxmlParente = null;
+        try {
+            fxmlParente = (Parent) FXMLLoader.load(arquivoFXML);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(fxmlParente, fxmlParente.getLayoutX(), fxmlParente.getLayoutY());
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
