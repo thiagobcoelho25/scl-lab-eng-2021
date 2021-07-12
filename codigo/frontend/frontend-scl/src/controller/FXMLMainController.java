95e60a2c18306d8deb73bf91ebe0efdd11f7688b
IsraelTuao <israeleduardomonteiro@gmail.com>
2021-07-12 15:54:17 -0300

    Inicio dos controllers-frontend

---
+++ codigo/frontend/frontend-scl/src/controller/FXMLMainController.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Israel
+ */
+public class FXMLMainController implements Initializable {
+
+    @FXML
+    private MenuItem Gerente;
+    @FXML
+    private MenuItem Funcionario;
+    @FXML
+    private MenuItem Cliente;
+    @FXML
+    private MenuItem Bairro;
+    @FXML
+    private MenuItem Produto;
+    @FXML
+    private MenuItem Ingrediente;
+    @FXML
+    private AnchorPane AnchorPanePrincipal;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void handleCadastroGerente() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroGerente.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+    
+    public void handleCadastroFuncionario() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroFuncionario.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+    
+    public void handleCadastroCliente() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroCliente.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+    
+    public void handleCadastroBairro() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroBairro.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+    
+    public void handleCadastroProduto() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroProduto.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+    
+    public void handleCadastroIngredientes() throws IOException{
+        AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/FXMLCadastroIngredientes.fxml"));
+        
+        AnchorPanePrincipal.getChildren().setAll(anchor);
+    
+    }
+}
--- codigo/frontend/frontend-scl/src/frontend/scl/FrontendScl.java
+++ codigo/frontend/frontend-scl/src/frontend/scl/FrontendScl.java
@@ -1,48 +1,23 @@
 package frontend.scl;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
 
 public class FrontendScl extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws Exception {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("/views/FXMLMain.fxml"));
+        Scene scene = new Scene(root);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        URL arquivoFXML = getClass().getResource("/views/FXMLCadastroPedido.fxml");
-
-        Parent fxmlParente = null;
-        try {
-            fxmlParente = (Parent) FXMLLoader.load(arquivoFXML);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene scene = new Scene(fxmlParente, fxmlParente.getLayoutX(), fxmlParente.getLayoutY());
-        
-        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+         
     }
 
     /**
